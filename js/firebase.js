fe9c1b5afe0a2c5b18b5ec00f189d73dae71a82c
Felipe Saenz <saenzfelipe2001@gmail.com>
2022-10-11 11:45:07 -0500

    Agregando los servicios de Firestore a la configuracion de Firebase del proyecto

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js';
 //metodo para el ingreso de usuarios
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-auth.js';
-
+//metodo para usar los servicios de Firestore
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js';
 
 
 const firebaseConfig = {
@@ -20,6 +21,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 //Initialize Autentication Login
 const auth = getAuth();
+// Initialize Firestore
+const db = getFirestore(app);
 
 export const loginValidation = (email,password) => signInWithEmailAndPassword(auth, email, password)
 
